596d65f1be138f561b7cfb4546fc66685621e8d9
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2024-01-26 11:14:01 +0700

    Navbar localStoraage Update 2.1: Stored user in state.

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,9 +4,12 @@ import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import { RiAddBoxLine, RiAddCircleLine } from "react-icons/ri";
 
+const [user, setUser] = useState("");
+
 export default function Navbar() {
   useEffect(() => {
-    const user = localStorage.getItem("name");
+    const storedUser = localStorage.getItem("name");
+    setUser(storedUser);
   }, []);
 
   return (
