66bd46a7f933260885629cc1f741bdd1e3b6bef2
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2024-03-14 23:47:22 +0700

    Updated navbar to show logged in email

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,33 +2,38 @@
 
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
-import { RiAddBoxLine, RiAddCircleLine } from "react-icons/ri";
+import { RiAddBoxLine } from "react-icons/ri";
+import { auth } from "@/libs/Firebase";
 
 export default function Navbar() {
-  const [user, setUser] = useState("");
+  const [userEmail, setUserEmail] = useState(null);
+
   useEffect(() => {
-    const storedUser = localStorage.getItem("name");
-    setUser(storedUser);
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        const userEmail = user.email;
+        setUserEmail(userEmail);
+        console.log("User has logged in");
+      } else {
+        setUserEmail(null);
+        console.log("User is logged out");
+      }
+    });
+
+    // Cleanup function
+    return () => unsubscribe();
   }, []);
 
   return (
-    // <nav className="flex justify-between items-center bg-slate-800 px-8 py-3">
-    //   <Link className="text-white font-bold" href={"/"}>
-    //     TBH-ChatBot Essentials
-    //   </Link>
-    //   <Link className="bg-white p-2" href={"/addModel"}>
-    //     Add Model
-    //   </Link>
-    // </nav>
-
     <nav className="flex justify-between items-center bg-slate-800 px-8 py-3">
       <Link className="text-white font-bold text-2xl pl-4" href={"/"}>
         TBH-ChatBot Essentials
-        {user && <p className="text-s">Logged in as {user}</p>}
+        {userEmail && (
+          <p className="text-sm font-normal">Logged in as {userEmail}</p>
+        )}
       </Link>
       <Link className="text-white text-bold p-2" href={"/addModel"}>
         <RiAddBoxLine size={60} />
-        {/* <RiAddCircleLine size={60} /> */}
       </Link>
     </nav>
   );
