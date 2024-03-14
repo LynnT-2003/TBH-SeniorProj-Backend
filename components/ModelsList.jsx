5888528aff54ea399fa4ea66a7210da4b6be71ad
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2024-03-15 00:39:01 +0700

    Added filter by shop

--- components/ModelsList.jsx
+++ components/ModelsList.jsx
@@ -4,7 +4,13 @@ import Link from "next/link";
 import RemoveBtn from "./RemoveBtn";
 import { HiPencilAlt } from "react-icons/hi";
 import { Grid, Item } from "@mui/material";
-import { Select, TextField, MenuItem, InputLabel, FormControl } from "@mui/material";
+import {
+  Select,
+  TextField,
+  MenuItem,
+  InputLabel,
+  FormControl,
+} from "@mui/material";
 import { useRouter } from "next/navigation";
 
 const getModels = async () => {
@@ -35,8 +41,8 @@ export default function ModelsList() {
   const [models, setModels] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  const keys = ["Brand", "Product_Name"]
-  const [query, setQuery] = useState("")
+  const keys = ["Brand", "Product_Name"];
+  const [query, setQuery] = useState("");
 
   const [selectedFilter, setSelectedFilter] = useState("");
 
@@ -46,20 +52,23 @@ export default function ModelsList() {
     // Redirect to login if not logged in
     const isLoggedIn = localStorage.getItem("isLoggedIn");
     if (!isLoggedIn || isLoggedIn === "false") {
-      router.push('/login'); // Redirect to login page
+      router.push("/login"); // Redirect to login page
     }
   }, [router]);
-  
+
   const search = (data) => {
     // return data.filter((item => keys.some((key) => item[key].toLowerCase().includes(query.toLowerCase()))))
-    return data.filter((item => {
+    return data.filter((item) => {
       // const brandMatch = item.Brand.toLowerCase().includes(query.toLowerCase());
       const keyMatch = keys.some((key) =>
         item[key].toLowerCase().includes(query.toLowerCase())
       );
-      const filterMatch = selectedFilter ? item.Brand === selectedFilter : true;
-      return keyMatch && filterMatch;}))
-  }
+      const filterMatch = selectedFilter
+        ? item.Brand === selectedFilter || item.Shop === selectedFilter
+        : true;
+      return keyMatch && filterMatch;
+    });
+  };
 
   const fetchModels = async () => {
     const fetchedModels = await getModels();
@@ -73,20 +82,10 @@ export default function ModelsList() {
 
   const signOut = () => {
     localStorage.clear();
-    router.push('/login');
+    router.push("/login");
   };
 
-  // if (!models) {
-  //   // You can optionally show a loading spinner or message here
-  //   return (
-  //     <>
-  //       <div className="text-center text-2xl font-bold">Loading...</div>
-  //     </>
-  //   );
-  // }
-
   if (loading) {
-    // Render the loading GIF while fetching tasks
     return (
       <div className="flex justify-center items-center">
         <img src="/loading.gif" alt="Loading..." />
@@ -94,66 +93,61 @@ export default function ModelsList() {
     );
   }
 
+  const shops = Array.from(new Set(models.map((m) => m.Shop)));
+  const brands = Array.from(new Set(models.map((m) => m.Brand)));
+
   return (
     <>
-    <div>
-      <Grid container spacing={2} className="">
-        <Grid xs={8} className="pl-4">
-          {/* <input
-            type="text"
-            placeholder="Search..."
-            className="px-2 border border-slate-800"
-            onChange={(e) => setQuery(e.target.value)}
-          /> */}
-          <TextField
+      <div>
+        <Grid container spacing={2} className="">
+          <Grid xs={6} className="pl-4">
+            <TextField
               size="small"
               label="Search..."
               variant="outlined"
+              style={{ width: "95%" }}
               onChange={(e) => setQuery(e.target.value)}
             />
-        </Grid>
-        <Grid xs={4} className="flex justify-end">
-          {/* <select className="border border-slate-800 py-0.5 pr-5" onChange={(e) => setSelectedFilter(e.target.value)}>
-            <option value="" disabled selected hidden>
-              Filter by...
-            </option>
-            <option value="Apple">Apple</option>
-            <option value="Samsung">Samsung</option>
-            <option value="Xiaomi">Xiaomi</option>
-          </select> */}
-          
-          <FormControl fullWidth
-              size="small">
-              <InputLabel>Filter by Brand:</InputLabel>
+          </Grid>
+          <Grid xs={6} className="flex justify-end">
+            <FormControl fullWidth size="small">
+              <InputLabel>Filter by Shop:</InputLabel>
               <Select
                 value={selectedFilter}
                 onChange={(e) => setSelectedFilter(e.target.value)}
-                label= "Filter by Brand:"
+                label="Filter by Brand:"
+                style={{ width: "95%" }}
               >
                 <MenuItem value="">
                   <em>None</em>
                 </MenuItem>
-                <MenuItem value="Apple">Apple</MenuItem>
-                <MenuItem value="Samsung">Samsung</MenuItem>
-                <MenuItem value="Xiaomi">Xiaomi</MenuItem>
+                {shops.map((shop) => (
+                  <MenuItem key={shop} value={shop}>
+                    {shop}
+                  </MenuItem>
+                ))}
               </Select>
             </FormControl>
-
-          {/* <FormControl fullWidth>
-            <InputLabel id="demo-simple-select-label">Age</InputLabel>
-            <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              label="Age"
-            >
-              <MenuItem value={10}>Ten</MenuItem>
-              <MenuItem value={20}>Twenty</MenuItem>
-              <MenuItem value={30}>Thirty</MenuItem>
-            </Select>
-          </FormControl> */}
+            <FormControl fullWidth size="small">
+              <InputLabel>Filter by Brand:</InputLabel>
+              <Select
+                value={selectedFilter}
+                onChange={(e) => setSelectedFilter(e.target.value)}
+                label="Filter by Brand:"
+              >
+                <MenuItem value="">
+                  <em>None</em>
+                </MenuItem>
+                {brands.map((brand) => (
+                  <MenuItem key={brand} value={brand}>
+                    {brand}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </Grid>
         </Grid>
-      </Grid>
-    </div>
+      </div>
       {search(models).map((m) => (
         <div
           className="px-12 p-4 border border-slate-300 my-3 flex justify-between gap-5"
@@ -177,7 +171,9 @@ export default function ModelsList() {
           </div>
         </div>
       ))}
-      <button className="border border-slate-500 px-2" onClick={signOut}>Sign out</button>
+      <button className="border border-slate-500 px-2" onClick={signOut}>
+        Sign out
+      </button>
     </>
   );
 }
